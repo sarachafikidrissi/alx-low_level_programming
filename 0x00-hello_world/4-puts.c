167efd80675e85e25122216cbca4023f300eda8f
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-06-15 12:27:57 -0700

    print a line using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-int puts("\"Programming is like building a multilingual puzzle");
+#include<stdio.h>
+/**
+ * main - write a program that prints a line using puts function
+ * Return: 0 
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle);
+return (0);
+}

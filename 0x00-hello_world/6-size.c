44f1aa95e0483dd94ce972962fe8ed895e3b8f0e
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-06-15 15:19:18 -0700

    printing the size of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of types
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf(sizeof(char));
+printf(sizeof(int));
+printf(sizeof(long int));
+printof(sizeof(long long int));
+printof(sizeof(float));
+return (0);
+}

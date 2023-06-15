fc94d179bc861bf36cb6e1c53aed3ad898feeb7d
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-06-15 14:22:04 -0700

    print a line using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line followed by a line using printf function
- * Return : returns 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {

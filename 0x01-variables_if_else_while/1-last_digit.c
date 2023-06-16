66b04bf5e253d34151858154c38a99716afaed70
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-06-16 12:10:19 -0700

    print last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ printf("Last digit of %d is %d and is 0\n", n, m);
 }
 else if (m < 6)
 {
-printf("Last digit of %d is %d and and is less than 6 and not 0\n", n, m);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 }
 return (0);
 }

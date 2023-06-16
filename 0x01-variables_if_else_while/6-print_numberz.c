05565455e8acea6da3f594b64b70be6b7e27f8b7
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-06-16 15:28:02 -0700

    print numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints numbers using putchar function
+ * Return: nummbers
+ */
+int main(void)
+{
+char c = '0';
+while (c <= '0')
+{
+	putchar(c);
+	c++
+}
+putchar('\n');
+return (0);
+}

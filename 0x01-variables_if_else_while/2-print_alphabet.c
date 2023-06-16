2718774494a60311535a6d2512cfe3abdc0e57da
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-06-16 12:22:17 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that prints alphabets using putchar function
+ * Return: an Alphabet character
+ */
+int main(0)
+{
+char c;
+c = 'a';
+while (c <= 'z')
+{
+	putchar("%c", c);
+	c++
+}
+return (0);

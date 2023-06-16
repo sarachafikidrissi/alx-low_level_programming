af1a1a4a3cece7d80c19f60179148d2930a3f3c6
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-06-16 13:43:17 -0700

    print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - a program that prints print alphabet except e and q
+ * Return: alphabets without e and q
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z' && !(c == 'e') && !(c == 'q'))
+{
+	putchar(c);
+	c++;
+}
+putchar('\n');
+return (0);
+}

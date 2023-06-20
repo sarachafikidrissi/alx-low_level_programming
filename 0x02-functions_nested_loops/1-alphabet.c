d88f8138b7c19a35bd922d1bae1c06fa5c52d9f0
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-06-20 10:35:00 -0700

    function that prints the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - A program that prints alphabets in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+	while (c <='z')
+	{
+		_putchar('c');
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
+

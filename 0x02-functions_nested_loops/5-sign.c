eec2b70b6172ff054c1dc280e999d3270a7ae06e
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-06-20 13:42:45 -0700

    sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,18 +10,21 @@ if (n > 0)
 {
 	_putchar('+');
 	_putchar(',');
+	_putchar(' ');
 	return (1);
 }
 else if (n == 0)
 {
 	_putchar('0');
 	_putchar(',');
+	_putchar(' ');
 	return (0);
 }
 else
 {
 	_putchar('-');
 	_putchar(',');
+	_putchar(' ');
 	return (-1);
 }
 }

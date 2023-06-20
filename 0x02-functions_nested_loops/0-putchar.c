8e17edc51811baa246bc662719b8845a9c54d2e2
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-06-20 10:13:59 -0700

    printing a string using _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,18 @@
-#include <stdio.h>
-#include <unistd.h>
+#include "main.h"
 /**
- * main - A program that prints a string using putchar only
+ * main - A program that prints a string using _putchar only
  * Return: Always 0
  */
-int c;
-#include "main.h"
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 return (0);
 }

58d88bb2fbae03792d07fdae1ef1b632caa37277
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-06-20 14:22:00 -0700

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,26 +1,17 @@
 #include "main.h"
 /**
  * _abs - A fucntion that computes the absolute value of a number
- * @abs: the absolute value
- * Return: 0
+ * @c: the number to be computed
+ * Return: the absolute value of nomber otherwise 0
  */
-int _abs(int)
+int _abs(int c)
 {
-int n;
-int abs;
+if (n < 0)
+{
+	int abs;
 
-if (n > 0)
-{
-	abs = n * 1;
-	_putchar(abs);
+	abs = c * -1;
+	return (abs);
 }
-else if (n < 0)
-{
-	abs = n * -1;
-	_putchar(abs);
-}
-else
-{
-	_putchar('0');
-}
+return (c);
 }

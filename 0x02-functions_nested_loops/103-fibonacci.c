c3cdc01fcbf427e7f4811b33230d68b413fb0ede
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-06-21 13:57:07 -0700

    3-fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -16,7 +16,7 @@ for (i = 2; i < 50; i++)
 fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 if ((fibonacci[i] % 2) == 0 && fibonacci[i] < 4000000)
 sum += fibonacci[i];
-}	
+}
 printf("%ld\n", sum);
 return (0);
 }

6d830a66305dc1354be706cd304aa0bf76ebbe77
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-06-20 18:14:36 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,25 +6,28 @@
  */
 void print_to_98(int n)
 {
-int i, j;
-if (n <= 98)
-{
-for (i = n; i <= 98; i++)
-{
-if (i != 98)
-printf("%d\n", i);
-else if (n == 98)
-printf("%d", i);
-}
-}
-else if (n >= 98)
-{
-for (j = n; j >= 98; j--)
-{
-if (j != 98)
-printf("%d", j);
-else if (j == 98)
-printf("%d", j);
-}
-}
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
 }

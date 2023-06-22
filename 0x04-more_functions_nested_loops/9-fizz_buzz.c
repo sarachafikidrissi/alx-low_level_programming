d69ecaf57aaaffbecb765531e8f79ccad94ded4c
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-06-22 14:08:33 -0700

    fizzfuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,17 +7,17 @@ int main(void)
 {
 	int n = 1;
 
-	for (n = 1; n <= 100; n++)
+	for (; n < 100; n++)
 	{
-		if (n % 3 == 0)
-			printf("Fuzz ");
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz ");
+		else if (n % 3 == 0)
+			printf("Fizz ");
 		else if (n % 5 == 0)
 			printf("Buzz ");
-		else if ((n % 3 == 0) && (n % 5 == 0))
-			printf("FuzzBuzz ");
 		else
 			printf("%d ", n);
 	}
-	printf("\n");
+	printf("Buzz\n");
 	return (0);
 }

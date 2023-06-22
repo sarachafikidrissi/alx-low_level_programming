1e8b233c34bdf35bc43b7279f40e26013921b517
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-06-22 15:08:58 -0700

    print triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,6 +9,8 @@ void print_triangle(int size)
 	int i;
 	int j;
 
+	if (size > 0)
+	{
 	for (i = 1; i <= size; i++)
 	{
 		for (j = 1; j <= size; j++)
@@ -20,4 +22,7 @@ void print_triangle(int size)
 		}
 		_putchar('\n');
 	}
+	}
+	else
+		_putchar('\n');
 }

f1fc606a355c2ecd9d2ec22427b94fe0f30ab7d9
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-06-21 18:37:36 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@
 void print_times_table(int n)
 {
 int i, j, result;
-if (n >= 0 && n <= 15)
+if (n > 0 && n < 15)
 {
 for (i = 0; i <= n; i++)
 {
@@ -16,7 +16,7 @@ for (i = 0; i <= n; i++)
 		result = i * j;
 		if (j == 0)
 			_putchar('0');
-		else if (result > 9 && result <= 99)
+		else if (result < 100)
 		{
 			_putchar(',');
 			_putchar(' ');

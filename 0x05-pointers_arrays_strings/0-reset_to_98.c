1e3ae617ad8cffe6aac700928b310c0a8c693371
sarachafikidrssi <chafikidrissisara@gmail.com>
2023-06-26 09:10:55 -0700

    update value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - updates the value of an integer to 98
- * @98: the updated value
+ * @n: the updated value
  * Return: 0 is successful
  */
 void reset_to_98(int *n)

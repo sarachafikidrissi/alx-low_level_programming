e6de7dcd5cdd57a1d917a18532e5e50d683e4957
sarachafikidrssi <chafikidrissisara@gmail.com>
2023-06-26 16:18:49 -0700

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,9 +8,11 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	char *aux = dest;
+	int i;
 
-	while (*src)
-		*dest++ = *src++;
-	return (aux);
+	for (i=0; i != '\0'; i++)
+		dest[i] = src [i];
+
+	dest[i++] = '\0';
+	return (dest);
 }

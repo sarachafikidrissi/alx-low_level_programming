76fddcb678d05ff4ef3372eea519b73628bc71b1
sarachafikidrssi <chafikidrsiisara@gmail.com>
2023-06-29 17:53:49 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,14 +8,14 @@ char *rot13(char *s)
 {
 	int i = 0;
 
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 	{
 		while ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z'))
 		{
 			if ((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm'))
 				s[i] = s[i] + 13;
 
-			else 
+			else
 				s[i] -= 13;
 
 			i++;

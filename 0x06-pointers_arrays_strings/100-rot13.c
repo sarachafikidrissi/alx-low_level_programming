76d40ad69a0938c286d0189722e61a1ccc521f88
sarachafikidrssi <chafikidrsiisara@gmail.com>
2023-06-29 17:50:07 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,2 +1,26 @@
 #include <stdio.h>
+/**
+ * rot13 - a function that encode a string to rot13
+ * @s: the string should be encoded
+ * Return: s
+ */
+char *rot13(char *s)
+{
+	int i = 0;
 
+	while(s[i] != '\0')
+	{
+		while ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z'))
+		{
+			if ((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm'))
+				s[i] = s[i] + 13;
+
+			else 
+				s[i] -= 13;
+
+			i++;
+		}
+		i++;
+	}
+	return (s);
+}

2c6491d9ceebc4842a8f6f4a66e06546bd54634d
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-07-07 11:25:00 -0700

    print name of program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - function that returns arguments
+ * @argc: number of parameters passed to the main function
+ * @argv: array of pointers
+ * Return: an integer
+ */
 int main(int __attribute__ ((unused)) argc, char *argv[])
 {
 	int i = 0;

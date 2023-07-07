28924b06efb20e6b7c91f997d98031eb55c4f1de
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-07-07 13:06:01 -0700

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,13 +8,14 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc == 1)
+	if (argc >= 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else if (argc > 1 && argc <= 3)
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	
 	return (0);
 }

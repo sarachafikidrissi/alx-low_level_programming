65d54e9d9e6f1446f5b9033d2629893bdc6c6310
sara chafik idrissi <chafikidrissisara@gmail.com>
2023-07-10 17:47:22 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strdup - function that returns a pointer that contains a copy of string
+ * @str: a pointer to string to be copied
+ * Return: pointer to the copied string
+ */
+char *_strdup(char *str)
+{
+	unsigned int len, i;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+	{
+		len++
+	}
+	ptr = malloc((len + 1) * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len ; i++)
+	{
+		s[i] = str[i];
+	}
+	return (s);
+}
+

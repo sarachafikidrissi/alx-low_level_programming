0a4588ac2c648620bfa4439d8617d90656b10aa6
sara chafik idrissi <chafikidrissisara@gmail.com>
2023-07-10 18:26:08 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,13 +19,17 @@ char *_strdup(char *str)
 		len++;
 	}
 	s = (char *) malloc((len + 1) * sizeof(char));
-
+	
+	if (s != NULL)
+	{
 	while (str[i] != '\0')
 	{
 		s[i] = str[i];
 		i++;
 	}
 	s[i] = '\0';
-	
 	return (s);
+	}
+	else
+		return (NULL);
 }

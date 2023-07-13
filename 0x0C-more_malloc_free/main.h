17fa76fecbb12816006e97ecee540808a83c4038
chafikidrissisara <chafikidrissisara@gmail.com>
2023-07-13 06:16:13 -0700

    header file

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef main_h
+#define main_h
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *malloc_checked(unsigned int b);
+
+#endif

1b05474f754ad7e9a0426d8ed050c2510111498d
chafikidrissisara <chafikidrissisara@gmail.com>
2023-07-17 15:14:41 -0700

    structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure data
+ * @name: First memeber
+ * @age: Second memeber
+ * @owner: last memeber
+ *
+ * Description: a structure that contains information of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

b8e9a272928f67c4ad4a8016338acc40635eb3df
chafikidrissisara <chafikidrissisara@gmail.com>
2023-07-17 18:14:58 -0700

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - a function that crreates a new dog
  * @name: name of dog
@@ -11,33 +12,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 	char *cpyname, *cpyowner;
-	int len_name = 0, len_owner = 0, i;
+	int len1, len2;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	while (name[len_name])
-		len_name++;
-	while (owner[len_owner])
-		len_owner++;
-
+	len1 = strlen(name);
+	len2 = strlen(owner);
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-
-	cpyname = malloc(len_name + 1);
+	cpyname = malloc(len1 + 1);
 	if (cpyname == NULL)
 		return (NULL);
-	for (i = 0; name[i]; i++)
-		cpyname[i] = name[i];
-	cpyname[i] = '\0';
+	cpyname = strcpy(cpyname, name);
 
-	cpyowner = malloc(len_owner + 1);
+	cpyowner = malloc(len2 + 1);
 	if (cpyowner == NULL)
 		return (NULL);
-	for (i = 0; owner[i]; i++)
-		cpyowner[i] = owner[i];
-	cpyowner[i] = '\0';
+	cpyowner = strcpy(cpyowner, owner);
 
 	new_dog->name = cpyname;
 	new_dog->age = age;

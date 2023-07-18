60c6b3243186b9c321a5630ed6d59a3f909a34b3
chafikidrissisara <chafikidrissisara@gmail.com>
2023-07-17 17:56:12 -0700

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - a function tha frees dogs
+ * @d: pointer to struct to free
+ * @return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		free(d->name);
+	if(d->owner != NULL)
+		free (d->owner);
+	free(d);
+}
+

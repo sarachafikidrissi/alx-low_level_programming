1b9f4ec8d79968bfe17079c6ac9d0d0bcc8ebcdc
chafikidrissisara <chafikidrissisara@gmail.com>
2023-07-31 18:14:33 +0100

    add at begining

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,11 +13,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	if (new_node == NULL)
 		return (NULL);
-	else
-	{
-		new_node->n = n;
-		new_node->next = *head;
-		*head = new_node;
-	}
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
 	return (new_node);
 }

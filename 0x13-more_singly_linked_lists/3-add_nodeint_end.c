3b98976eee37e7dbba286bbf91951bc0670d496e
chafikidrissisara <chafikidrissisara@gmail.com>
2023-08-01 01:09:21 +0100

    add node at end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,6 +4,7 @@
  * a new node at the end of a listint_t list
  * @head: pointer to the adresse of newnode
  * @n: the number to be printed
+ * Return: the address of the new element, or NULL if it failed
 */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {

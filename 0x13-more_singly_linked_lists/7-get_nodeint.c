ce59e00c07fbd51fa0cb65a223d0026a7bbec097
chafikidrissisara <chafikidrissisara@gmail.com>
2023-07-31 19:29:25 -0700

    get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,13 +4,15 @@
  * node of a listint_t linked list
  * @head: pointer to node
  * @index: position of a node
- * Return : node at index position
+ * Return: node at index position
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *temp;
-	int i = 1;
+	unsigned int i = 1;
 
+	if (head == NULL)
+		return (NULL);
 	temp = head;
 
 	while (i <= index)

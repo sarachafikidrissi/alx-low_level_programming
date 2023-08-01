5c32fe506d2ad52b90703611a09f99bd3682a42b
chafikidrissisara <chafikidrissisara@gmail.com>
2023-08-01 16:52:29 -0700

    printlistsafe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,9 +9,10 @@ size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *temp = head, *next_node = head;
 	size_t count = 0;
+
 	if (head == NULL)
 		exit(98);
-	
+
 	while (temp && next_node && next_node->next && head)
 	{
 		temp = temp->next;
@@ -19,10 +20,10 @@ size_t print_listint_safe(const listint_t *head)
 
 		if (temp == next_node)
 		{
-			printf("[%p] %d\n", (void *)head, head->n);
+			printf("-> [%p] %d\n", (void *)head, head->n);
 			exit(98);
 		}
-		printf("[%p] %d", (void *)head, head->n);
+		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
 		count++;
 	}

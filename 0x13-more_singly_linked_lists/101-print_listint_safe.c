8f6cd9289bf256ba4602385de539d83636fc0e10
chafikidrissisara <chafikidrissisara@gmail.com>
2023-08-03 07:32:38 +0100

    0x14-bit_manipulation

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,9 +9,10 @@ size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *temp = head, *next_node = head;
 	size_t count = 0;
+
 	if (head == NULL)
 		exit(98);
-	
+
 	while (temp && next_node && next_node->next && head)
 	{
 		temp = temp->next;
@@ -29,4 +30,3 @@ size_t print_listint_safe(const listint_t *head)
 	head = NULL;
 	return (count);
 }
-

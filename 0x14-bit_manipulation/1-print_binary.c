610a2ca5eebb200eb3cab99279b3c9962395adc3
chafikidrissisara <chafikidrissisara@gmail.com>
2023-08-03 10:21:28 +0100

    fixing some errors

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,32 +7,24 @@
 */
 void print_binary(unsigned long int n)
 {
-	unsigned int mask;
-	int bits;
+	unsigned long int mask = sizeof(n) * 8;
 
     if (n == 0)
         _putchar('0');
     else if (n == 1)
         _putchar('1');
-
-    else
+	
+	else
     {
-        if (n < 100)
-            bits = 7;
-        else if (n >= 100)
-            bits = 11;
-
-	    mask = 1 << (bits - 1);
-
-	    while (mask > 0)
-	    {
-		    if (n & mask)
-			    _putchar('1');
+		mask = 1 << (bits - 1);
 		
-		    else
-    	        _putchar('0');
-
-            mask >>= 1;
-	    }
-    }
+		while (mask > 0)
+		{
+			if (n & mask)
+				_putchar('1');
+			else
+				_putchar('0');
+			mask >>= 1;
+		}
+	}
 }

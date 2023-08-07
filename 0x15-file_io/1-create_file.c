b1565d17063e48c65d26abf56008f59bd5d6405e
chafikidrissisara <chafikidrissisara@gmail.com>
2023-08-07 22:14:22 +0100

    added task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * create_file -  a function that creates a file
- * @filrname: the filename to create
- * @text_conternt: a NULL terminated string to write to the file
- * Returns: 1 on success, -1 on failure (file can not be created,
+ * @filename: the filename to create
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure (file can not be created,
  * file can not be written, write “fails”, etc…)
 */
 int create_file(const char *filename, char *text_content)
 {
-	int fo,fw, len = 0;
+	int fo, fw, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	while (text_content && *(text_content + len))
 		len++;
 	fw = write(fo, text_content, len);
-	close (fo);
+	close(fo);
 	if (fw < 0)
 		return (-1);
 	return (1);
